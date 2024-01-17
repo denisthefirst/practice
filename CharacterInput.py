02e4879680d589246f407121ff2fbfbd688c67e9
Denis <denismaric2000@gmail.com>
2024-01-17 12:27:00 +0100

    all

--- CharacterInput.py
+++ CharacterInput.py
@@ -3,10 +3,8 @@ from datetime import date
 name = input("Give me your name: ")
 age = int(input("Give me your age: "))
 printing = int(input("Give me the amount of how many times you want this messages to shown: "))
-current_year = date.today().year
-years_until_100 = 100 - age
-years_turn_100 = current_year + years_until_100
+years_until_100 = date.today().year + (100 - age)
 
 for i in range(printing):
-    print("Your name is " + name + "! " + "In year " + str(years_turn_100)
+    print("Your name is " + name + "! " + "In year " + str(years_until_100)
         + " you are going to turn 100 years old!")
