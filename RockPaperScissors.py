1a97aff4174c8bb77c1f3f400a8151ab3b8ab5ce
Denis <denismaric2000@gmail.com>
2024-01-17 20:18:51 +0100

    napola

---
+++ RockPaperScissors.py
@@ -0,0 +1,7 @@
+player1 = input("Enter first players name: ")
+player2 = input("Enter second players name: ")
+
+game = ["Rock", "Paper", "Scissors"]
+
+game1 = input(player1 + "´s turn: ")
+game2 = input(player2 + "´s turn: ")
