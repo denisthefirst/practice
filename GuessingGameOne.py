3e17a14d6dfc62c3e496609ca711b13b67604061
Denis <denismaric2000@gmail.com>
2024-01-19 11:45:39 +0100

    exercise 9 done

---
+++ GuessingGameOne.py
@@ -0,0 +1,19 @@
+import random
+
+num = int(input("Guess the number from 1-9: "))
+randomNum = random.randint(1,9)
+i = 1
+
+while num != randomNum:
+    num = int(input("Guess the number from 1-9: "))
+    i += 1
+    if num < randomNum:
+        num = int(input("Ouh, you guessed to low! Try again: "))
+    else:
+        num = int(input("Ouh, you guessed to high! Try again: "))
+
+print("Congrats! You guessed right: " + str(num) + "=" + str(randomNum) + "\n It took you: " + str(i) + " guesses")
+
+
+
+
