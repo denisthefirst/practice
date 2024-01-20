9c2f575e35c1fd8935917482697233df61700d54
Denis <denismaric2000@gmail.com>
2024-01-20 12:42:24 +0100

    changes comitted

--- ListOverlapComprehensions.py
+++ ListOverlapComprehensions.py
@@ -1,10 +1,6 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+import random
 
-list = []
-
-for i in range(len(a)):
-    if a[i] in b and a[i] not in list:
-        list.append(a[i])
-
-print(list)
+a = random.sample(range(1, 30), 12)
+b = random.sample(range(1, 30), 16)
+result = [i for i in a if i in b]
+print(result)
